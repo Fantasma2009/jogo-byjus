a099346f00c011abc427625be774852a1babbbe1
Fantasma2009 <95443797+Fantasma2009@users.noreply.github.com>
2022-09-13 15:51:26 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var cacador1,cacador2,cacador3;
+var navio;
+var indio;
+var imgdefundo;
+var balacacador;
+var dardo;
+
+function preload(){
+    cacador1=loadImage("cacador1.png");
+    cacado2=loadImage("cacador2.png");
+    cacador3=loadImage("cacador3.png");
+    navio=loadImage("barco.png");
+    dardo=loadImage("zarabatana.png");
+    bala=loadImage("bala.png");
+    indio=loadImage("indio.png");
+    imgdefundo=loadImage("background.gif");
+
+}
+
+function setup(){
+
+
+
+
+    
+}
