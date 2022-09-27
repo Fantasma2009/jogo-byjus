ee997698049f70952175f1516084708b4bb54b58
Fantasma2009 <95443797+Fantasma2009@users.noreply.github.com>
2022-09-27 15:46:38 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,84 @@
+var cacador1,cacador2,cacador3;
+var navio;
+var indio;
+var imgdefundo;
+var balacacador;
+var dardo;
+var cacador1img,cacador2img,cacador3img;
+var navioimg;
+var indioimg;
+var imgdefundoimg;
+var balacacadorimg;
+var dardoimg;
+
+function preload(){
+    cacador1img=loadImage("cacador1.png");
+    cacador2img=loadImage("cacador2.png");
+    cacador3img=loadImage("cacador3.png");
+    navioimg=loadImage("barco.png");
+    dardoimg=loadImage("zarabatana.png");
+    balacacadorimg=loadImage("bala.png");
+    indioimg=loadImage("indio.png");
+    imgdefundoimg=loadImage("background.gif");
+    dardoimg=loadImage("zarabatana.png");
+
+}
+
+function setup(){
+  createCanvas(1500,750);
+    cacador1=createSprite(1440,80,50,60);
+    cacador1.addImage(cacador1img);
+    cacador2=createSprite(1240,80,50,60);
+    cacador2.addImage(cacador2img);
+    cacador3=createSprite(1140,80,50,60);
+    cacador3.addImage(cacador3img);
+    navio=createSprite(1200,500,140,100);
+    navio.addImage(navioimg);
+    navio.scale=2.2;
+    indio=createSprite(75,400,60,40);
+    indio.addImage(indioimg);
+    indio.scale=0.6;
+    
+}
+
+function draw(){
+background(imgdefundoimg);
+
+if(keyDown(UP_ARROW)){
+
+  indio.y=indio.y-4.9;
+
+}
+
+if(keyDown(DOWN_ARROW)){
+
+  indio.y=indio.y+4.9;
+
+}
+
+
+if(keyDown(32)){
+
+setTimeout(function atirardardos(){
+  dardo=createSprite(indio.x+135,indio.y,20,20);
+  dardo.addImage(dardoimg);
+  dardo.scale=0.6;
+
+  dardo.velocityX=dardo.velocityX+4.7;
+
+},3000);
+
+
+}
+
+
+drawSprites();
+}
+
+/*function atirardardos(){
+
+ 
+
+
+
+}*/
